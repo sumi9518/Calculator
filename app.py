3114c26f8cf935828c042258f2c393c6d5d80499
Suman Yadav <sumanyadav9518@gmail.com>
2026-03-31 16:50:52 +0200

    Converted the input to number

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 print("Simple Calculator")
 
-num1 = input("Enter the first number : ")
-num2 = input("Enter the second number : ")
+num1 = float(input("Enter the first number : "))
+num2 = float(input("Enter the second number : "))
 
-print(num1, num2)
+print(num1+num2)

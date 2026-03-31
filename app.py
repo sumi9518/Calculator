467984fbec8a45adefeb00f9529c3424681e1792
Suman Yadav <sumanyadav9518@gmail.com>
2026-03-31 16:59:58 +0200

    Added basic operations to calculator

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 print("Simple Calculator")
 
 num1 = float(input("Enter the first number : "))
+operator = input("Enter the operator (+,-,*,/) : ")
 num2 = float(input("Enter the second number : "))
 
-print(num1+num2)
+if operator == "+":
+    print(num1+num2)
+
+elif operator == "-":
+    print(num1-num2)
+
+elif operator == "*":
+    print(num1*num2)
+
+elif operator == "/" :
+    print(num1/num2)
+
+else:
+    print("Invalid Operator")

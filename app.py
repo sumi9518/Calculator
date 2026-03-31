ed6c8796b778c3a9915f67a924ddae7ff2643dce
Suman Yadav <sumanyadav9518@gmail.com>
2026-03-31 17:26:36 +0200

    Add input validation and division by zero handling

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 print("Simple Calculator")
 
-num1 = float(input("Enter the first number : "))
-operator = input("Enter the operator (+,-,*,/) : ")
-num2 = float(input("Enter the second number : "))
+try:
+    num1 = float(input("Enter the first number : "))
+    operator = input("Enter the operator (+,-,*,/) : ")
+    num2 = float(input("Enter the second number : "))
 
-if operator == "+":
-    print(num1+num2)
+    if operator == "+":
+        print(num1+num2)
 
-elif operator == "-":
-    print(num1-num2)
+    elif operator == "-":
+        print(num1-num2)
 
-elif operator == "*":
-    print(num1*num2)
+    elif operator == "*":
+        print(num1*num2)
 
-elif operator == "/" :
-    print(num1/num2)
+    elif operator == "/" :
+        if num2 == 0:
+            print("Cannot divide by 0")
+        else:
+            print(num1/num2)
+    else:
+        print("Invalid Operator")
 
-else:
-    print("Invalid Operator")
+except ValueError:
+    print("Please enter a valid number")
